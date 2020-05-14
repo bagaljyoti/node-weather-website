c35f76fa22a1e69ea0fd03f78cbb5fa01b894f25
jyoti bagal <bagaljyoti510@gmail.com>
2020-05-14 11:23:19 +0530

    changes

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -9,7 +9,7 @@ request({url, json: true}, (error,{body}) => {
     }else if (body.error){
         callback('Unable to find location', undefined)
     }else {
-        callback(undefined,body.current.weather_descriptions[0] + ". It is Currently " + body.current.temperature + " degrees out. There is " +body.current.precip + "% chance of rain.")
+        callback(undefined,body.current.weather_descriptions[0] + ". It is Currently " + body.current.temperature + " degrees out. There is + " +body.current.precip + "% chance of rain. The Humidity is "+ body.current.humidity +"%.")
     }
 })
 }
